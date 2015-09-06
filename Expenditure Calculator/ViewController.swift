bdc6ccb1f15d0ba0bd995bd300cf5500f545ca1c
Arin Houck <arinhouck@gmail.com>
2015-09-06 12:39:36 -0700

    Initial Commit

---
+++ Expenditure Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Expenditure Calculator
+//
+//  Created by Arin Houck on 9/6/15.
+//  Copyright (c) 2015 Arin Houck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
